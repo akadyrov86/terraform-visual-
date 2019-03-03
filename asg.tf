b54e71552dfe93448210247c0427574aee43ee14
AKMAL KADYROV <AKMALKADYROV@AKMALs-MacBook-Pro.local>
2019-03-03 15:08:42 -0700

    lover case

--- asg.tf
+++ asg.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscalling_group" "dev" {
     name = "dev"
     launch_configuration = "${aws_launch_autoscalling_group.dev.name}"
-    min = "${var.min.size}"
-    max = "${var.max.size}"
+    min_size = "${var.min.size}"
+    max_size = "${var.max.size}"
 
 
 }
