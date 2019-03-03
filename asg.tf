8b37a6dbdcd9e9240c70e4498aa373f5dc1e7feb
AKMAL KADYROV <AKMALKADYROV@AKMALs-MacBook-Pro.local>
2019-03-03 15:54:30 -0700

    data deleted

--- asg.tf
+++ asg.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_group" "dev" {
     name = "dev"
-    launch_configuration = "${aws_launch_autoscaling_group.dev.name}"
+    launch_configuration = "${aws_launch_autoscaling.dev.name}"
     min_size = "${var.min_size}"
     max_size = "${var.max_size}"
     availability_zones = ["eu-west-1a","eu-west-1b","eu-west-1c"]
