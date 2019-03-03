b3b65283a72363e955dba6cc7cb5c54e52888494
AKMAL KADYROV <AKMALKADYROV@AKMALs-MacBook-Pro.local>
2019-03-03 15:06:13 -0700

    name chaned

--- asg.tf
+++ asg.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscalling_group" "dev" {
     name = "dev"
-    launch_configuration = "${aws_autoscalling_group.dev.name}"
+    launch_configuration = "${aws_launch_autoscalling_group.dev.name}"
     min = "${var.min.size}"
     max = "${var.max.size}"
 
