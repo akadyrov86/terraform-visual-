2338e558e63fced064faf898895ad1219177b077
AKMAL KADYROV <AKMALKADYROV@AKMALs-MacBook-Pro.local>
2019-03-03 15:22:54 -0700

    _ added

--- asg.tf
+++ asg.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscalling_group" "dev" {
     name = "dev"
     launch_configuration = "${aws_launch_autoscalling_group.dev.name}"
-    min_size = "${var.min.size}"
-    max_size = "${var.max.size}"
+    min_size = "${var.min_size}"
+    max_size = "${var.max_size}"
     availability_zones = ["eu-west-1a","eu-west-1b","eu-west-1c"]
 
 
